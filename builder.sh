39bbfff93a2915f344c9abb7ec3ba492d0f3bd7a
Abdulkerim Ekrem Bekir <aekrembekir@gmail.com>
2022-10-04 00:52:45 +0300

    Update builder.sh

--- builder.sh
+++ builder.sh
@@ -3,7 +3,7 @@
 sudo apt update
 sudo apt install -y gcc make libgmp-dev unzip
 cd $HOME
-rm -f main.zip
+rm -i main.zip
 wget https://github.com/Aerbil/Time-lock-encryption/archive/main.zip
 unzip -o main.zip
 rm -f main.zip
