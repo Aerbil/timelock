2abb532717a059538c253aa44f583a92310ce5ec
Abdulkerim Ekrem Bekir <aekrembekir@gmail.com>
2022-10-04 00:44:09 +0300

    Create builder.sh

---
+++ builder.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt install -y gcc make libgmp-dev unzip
+cd $HOME
+rm -f main.zip
+wget https://github.com/Aerbil/Time-lock-encryption/archive/main.zip
+unzip -o main.zip
+rm -f main.zip
+cd Time-lock-encryption-main/src
+make
